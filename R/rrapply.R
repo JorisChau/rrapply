1c5b3adc5ce0e0c6ef2c260ed95c77721ef87c6e
JorisChau <joris.chau@openanalytics.eu>
2020-07-04 22:19:00 +0200

    Edit examples

--- R/rrapply.R
+++ R/rrapply.R
@@ -234,10 +234,12 @@
 #' # Applying functions to data.frames
 #' 
 #' ## Scale only Sepal columns in iris dataset
-#' rrapply(
-#'   iris,
-#'   condition = function(x, .xname) grepl("Sepal", .xname),
-#'   f = scale
+#' head(
+#'   rrapply(
+#'     iris,
+#'     condition = function(x, .xname) grepl("Sepal", .xname),
+#'     f = scale
+#'   )
 #' )
 #' 
 #' ## Scale and keep only numeric columns
